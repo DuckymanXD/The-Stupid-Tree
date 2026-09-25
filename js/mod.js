f73f4dfd64a69d0bdb82c28be8707302d9451ad7
DuckymanXD <duckleader6@gmail.com>
2026-09-25 18:40:02 +0000

    fix: Update modInfo with discord name and correct version name

--- js/mod.js
+++ js/mod.js
@@ -4,7 +4,7 @@ let modInfo = {
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
+	discordName: "duckyman9",
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
@@ -13,13 +13,12 @@ let modInfo = {
 // Set your version in num and name
 let VERSION = {
 	num: "0.0",
-	name: "Literally nothing",
+	name: "Start I Guess",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Started`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
